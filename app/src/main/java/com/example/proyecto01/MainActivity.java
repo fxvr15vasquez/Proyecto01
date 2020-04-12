8cdcc2e7ccfd15fb1efae0606ff4d1e1359e91ea
fxvr15vasquez <57450853+fxvr15vasquez@users.noreply.github.com>
2020-04-12 16:58:05 -0500

    vasquez
    cambio del main

--- app/src/main/java/com/example/proyecto01/MainActivity.java
+++ app/src/main/java/com/example/proyecto01/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.proyecto01.modeloDB.usuarioDB;
 
@@ -37,9 +38,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        String user = lguser.getText().toString();
+        String pass = lgpass.getText().toString();
+        int id_usu = usuDB.login(user, pass);
         switch (v.getId()){
             case R.id.btningre:
+                if (user.equals("") && pass.equals("")){
+                    Toast.makeText(this,"Campos vacios",Toast.LENGTH_SHORT).show();
+                }else if(id_usu !=0){
+                    Intent inlogin = new Intent(MainActivity.this,Inicio.class);
+                    inlogin.putExtra("id_usu",id_usu);
+                    startActivity(inlogin);
+                }else{
+                    Toast.makeText(this,"Usuario o contraseña incorrectas",Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.btnregis:
                 Intent inregis = new Intent(MainActivity.this,Registro.class);
